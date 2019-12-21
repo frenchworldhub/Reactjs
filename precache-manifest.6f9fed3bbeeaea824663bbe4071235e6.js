a4c9622f3372b8a3f69c1464898e86c82d95c567
Akash Bisht <you@example.com>
2019-12-21 14:14:11 +0530

    Updates

---
+++ precache-manifest.6f9fed3bbeeaea824663bbe4071235e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a60494fc194582aa40390ec36f57fb7",
+    "url": "/Reactjs/index.html"
+  },
+  {
+    "revision": "d4890725d5c33db81973",
+    "url": "/Reactjs/static/css/main.bec4570a.chunk.css"
+  },
+  {
+    "revision": "70f6a5b6459dd8425676",
+    "url": "/Reactjs/static/js/2.c8e3e156.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Reactjs/static/js/2.c8e3e156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4890725d5c33db81973",
+    "url": "/Reactjs/static/js/main.030a5c89.chunk.js"
+  },
+  {
+    "revision": "2b10134ef42fc71943e2",
+    "url": "/Reactjs/static/js/runtime-main.23fae76c.js"
+  },
+  {
+    "revision": "b2034cb9c58a9a12ddd67d69168dab98",
+    "url": "/Reactjs/static/media/as.b2034cb9.jpg"
+  }
+]);
